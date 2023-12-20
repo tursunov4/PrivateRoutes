15d8c4c2a1a46a6b137b5ef096b0cdeb5a9ee43b
tursunov4 <tursunovsh4@gmail.com>
2023-12-20 15:42:17 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import PrivateRoutes from './Untils/ProtectedRoutes'
+import Users from './Users';
+import Product from './Product';
+import Login from './Login';
+import NotFound from './NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route element={<PrivateRoutes/>}>
+            <Route path='*' element={<NotFound/>} />
+              <Route path='/' element={<Users/>} />
+              <Route path='/products' element={<Product/>} />
+          </Route>
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
+    </Router>
   );
 }
-
-export default App;
+export default App
